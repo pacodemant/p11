6ab9f21b7b5f79061cdf337527573e939fc31bef
pacodemant <148328057+pacodemant@users.noreply.github.com>
2024-01-30 21:16:12 +0100

    Projektarbeit P11

---
+++ bin/main.dart
@@ -0,0 +1,59 @@
+class User {
+  String username;
+  String password;
+  String email;
+  int id;
+  int age;
+
+  User(this.username, this.password, this.email, this.id, this.age);
+
+  // Methoden: SignIn und LogIn
+  void signIn() {
+    print('Anmeldung $username.');
+  }
+
+  void logIn() {
+    print('login $username.');
+  }
+}
+
+class MockDatabaseRepository {
+  // private attribute
+  //! Warum schlägt mir Dart vor, folg. Liste FINAL zu machen?
+  final List<User> _users = [
+    User('Kai', 'pass123', 'kai@example.com', 1, 25),
+    User('Angi', 'pass456', 'angi@example.com', 2, 30),
+    User('David', 'pass789', 'david@example.com', 3, 28),
+  ];
+
+  // constructor
+  MockDatabaseRepository();
+
+  // access methods / functions
+  List<User> getUsers() {
+    return _users;
+  }
+
+  void addUser(User user) {
+    _users.add(user);
+  }
+
+  void deleteUser(User user) {
+    _users.remove(user);
+  }
+
+  // Main method
+}
+
+void main() {
+  // create instance of DatabaseRepository
+  MockDatabaseRepository mockDatabaseRepository = MockDatabaseRepository();
+
+  // get all users
+  List<User> users = mockDatabaseRepository.getUsers();
+  for (User user in users) {
+    print('${user.username}, ${user.email}, ${user.age} years old');
+    user.signIn();
+    user.logIn();
+  }
+}
---
+++ bin/p11.dart
@@ -0,0 +1,5 @@
+import 'package:p11/p11.dart' as p11;
+
+void main(List<String> arguments) {
+  print('Hello world: ${p11.calculate()}!');
+}
